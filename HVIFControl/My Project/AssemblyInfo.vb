b8b97b798a70b9ba594c3dcb8b7ff8151bacf766
Joachim Mairböck <j.mairboeck@gmail.com>
2022-03-26 19:20:05 +0100

    move the HVIFControl to a separate library to be able to use it independently from the viewer application.

---
+++ HVIFControl/My Project/AssemblyInfo.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("HVIFControl")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("HVIFControl")>
+<Assembly: AssemblyCopyright("Copyright @  2022")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: ComVisible(false)>
+
+'<Assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)>
+
+<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
+
+<Assembly: Guid("ed3f32b2-5630-467f-a0ec-4d169a7259b3")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
